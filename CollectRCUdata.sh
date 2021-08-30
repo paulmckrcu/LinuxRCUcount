4a11542395eaa850f989da57c47fbd3083ce628c
Paul E. McKenney <paulmck@kernel.org>
2021-08-29 21:27:04 -0700

    CollectRCUdata: A few more kernel releases...
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- CollectRCUdata.sh
+++ CollectRCUdata.sh
@@ -150,4 +150,9 @@ destdir=${1-~/paper/RCU/intro/linuxusage}
 # sh LinuxRCUcount.sh 5.6 ${destdir}
 # sh LinuxRCUcount.sh 5.7 ${destdir}
 # sh LinuxRCUcount.sh 5.8 ${destdir}
-sh LinuxRCUcount.sh 5.9 ${destdir}
+# sh LinuxRCUcount.sh 5.9 ${destdir}
+# sh LinuxRCUcount.sh 5.10 ${destdir}
+# sh LinuxRCUcount.sh 5.11 ${destdir}
+# sh LinuxRCUcount.sh 5.12 ${destdir}
+# sh LinuxRCUcount.sh 5.13 ${destdir}
+sh LinuxRCUcount.sh 5.14 ${destdir}
